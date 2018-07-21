c94e28d548f2bc0233071c63d5dcf97aa5d79fa8
Tamas Blummer <tamas.blummer@gmail.com>
2018-07-21 12:43:00 +0200

    iterate over data elements

--- src/datafile.rs
+++ src/datafile.rs
@@ -22,9 +22,10 @@ use asyncfile::AsyncFile;
 use blockdb::{RW,DBFile,BlockIterator,BlockFile};
 use block::{Block, PAYLOAD_MAX};
 use error::BCSError;
-use types::Offset;
+use types::{Offset, U24};
 
 use std::sync::Arc;
+use std::cmp::min;
 
 /// The key file
 pub struct DataFile {
@@ -44,11 +45,11 @@ impl DataFile {
         self.async_file.shutdown()
     }
 
-    fn block_iter (&self) -> BlockIterator {
+    pub fn block_iter (&self) -> BlockIterator {
         BlockIterator::new(self)
     }
 
-    fn data_iter (&self) -> DataIterator {
+    pub fn data_iter (&self) -> DataIterator {
         DataIterator::new(self.block_iter())
     }
 }
@@ -78,6 +79,7 @@ impl BlockFile for DataFile {
 }
 
 /// types of data stored in the data file
+#[derive(Eq, PartialEq)]
 pub enum DataType {
     /// no data, just padding the storage blocks with zero bytes
     Padding,
@@ -87,6 +89,26 @@ pub enum DataType {
     HeaderOrBlock,
     /// Spillover bucket of the hash table
     TableSpillOver
+}
+
+impl DataType {
+    pub fn from (data_type: u8) -> DataType {
+        match data_type {
+            1 => DataType::TransactionOrAppData,
+            2 => DataType::HeaderOrBlock,
+            3 => DataType::TableSpillOver,
+            _ => DataType::Padding
+        }
+    }
+
+    pub fn to_u8 (&self) -> u8 {
+        match self {
+            DataType::Padding => 0,
+            DataType::TransactionOrAppData => 1,
+            DataType::HeaderOrBlock => 2,
+            DataType::TableSpillOver => 3
+        }
+    }
 }
 
 pub struct DataEntry {
@@ -104,6 +126,51 @@ impl<'file> DataIterator<'file> {
     pub fn new (block_iterator: BlockIterator<'file>) -> DataIterator {
         DataIterator{block_iterator, pos: 0, current: None}
     }
+
+    fn skip_padding(&mut self) {
+        loop {
+            if let Some(ref mut current) = self.current {
+                while self.pos < PAYLOAD_MAX &&
+                    DataType::from(current.payload[self.pos]) == DataType::Padding {
+                    self.pos += 1;
+                }
+            }
+            else {
+                break;
+            }
+            if self.pos == PAYLOAD_MAX {
+                self.current = self.block_iterator.next();
+                self.pos = 0;
+            }
+            else {
+                break;
+            }
+        }
+    }
+
+    fn read_slice (&mut self, slice: &mut [u8]) -> bool {
+        let mut read = 0;
+        loop {
+            let mut have = min(PAYLOAD_MAX - self.pos, slice.len() - read);
+            if let Some(ref mut current) = self.current {
+                slice[read .. read + have].copy_from_slice(&current.payload[self.pos .. self.pos + have]);
+                self.pos += have;
+                read += have;
+
+                if read == slice.len() {
+                    return true;
+                }
+            }
+            else {
+                return false;
+            }
+            if have == 0 {
+                self.current = self.block_iterator.next();
+                self.pos = 0;
+                have = min(PAYLOAD_MAX, slice.len() - read);
+            }
+        }
+    }
 }
 
 impl<'file> Iterator for DataIterator<'file> {
@@ -112,12 +179,47 @@ impl<'file> Iterator for DataIterator<'file> {
     fn next(&mut self) -> Option<Self::Item> {
         if self.current.is_none() {
             self.current = self.block_iterator.next();
+            // skip magic on first block
             self.pos = 2;
         }
-        if self.current.is_none() {
-            return None;
+        if self.current.is_some() {
+            self.skip_padding();
+
+            let mut dt = [0u8; 1];
+            if self.read_slice(&mut dt) {
+                let data_type = DataType::from(dt[0]);
+                let mut size = [0u8; 6];
+                if self.read_slice(&mut size) {
+                    let len = U24::from_slice(&size).unwrap();
+                    let mut buf = vec!(0u8; len.as_usize());
+                    if self.read_slice(buf.as_mut_slice()) {
+                        return Some(DataEntry { data_type, content: buf });
+                    }
+                }
+            }
         }
-        // TODO
         None
+    }
+}
+
+#[cfg(test)]
+mod test {
+    extern crate hex;
+
+    use super::*;
+    use inmemory::InMemory;
+
+    #[test]
+    fn test() {
+        let mem = InMemory::new(true);
+        let mut data = DataFile::new(Box::new(mem));
+        assert!(data.block_iter().next().is_none());
+        assert!(data.data_iter().next().is_none());
+        let mut block = Block::new(Offset::new(0).unwrap());
+        block.append(&[0xBC, 0xDA]).unwrap();
+        data.append_block(Arc::new(block));
+
+        data.flush().unwrap();
+        data.sync().unwrap();
     }
 }
