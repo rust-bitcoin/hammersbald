ac4c3b4ab0a70c071bc9f85eaa1a829d85aab4e3
Tamas Blummer <tamas.blummer@gmail.com>
2018-09-26 11:45:06 +0200

    benchmark with 100 million transactions

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,72 +1,68 @@
 extern crate blockchain_store;
 extern crate bitcoin;
-extern crate rand;
 
 use blockchain_store::infile::InFile;
 use blockchain_store::bcdb::BCDBFactory;
 
 use bitcoin::util::hash::Sha256dHash;
 
-use rand::thread_rng;
-use rand::RngCore;
-
 use std::time::{Instant};
+use std::collections::HashSet;
+use std::mem::transmute;
 
 pub fn main () {
     let mut db = InFile::new_db("testdb").unwrap();
     db.init().unwrap();
 
     // transaction size assumed 300 bytes
-    let mut data = [0x0u8;300];
+    let data = [0x0u8;300];
 
-    // number of transactions
-    let ntx = 50000000;
+    // simulating a blockchain ingest
+
+    // number of transactions (100 million)
+    let ntx = 100000000;
     // transactions per block
     let tb = 1000;
-    // download batch size (number of blocks)
+    // load batch size (in number of blocks)
     let bat = 1000;
 
-    let mut rng = thread_rng();
+    // generate unique keys
+    println!("Generate keys ...");
+    let mut keys = HashSet::new();
+    for i in 1 .. ntx {
+        let bytes: [u8; 8] = unsafe { transmute(i) };
+        let hash = Sha256dHash::from_data(&bytes);
+        let key = hash.data();
+        keys.insert (key);
+    }
+
+
+    println!("Inserting data ...");
     let mut n = 0;
     let mut now = Instant::now();
-    for i in 1 .. ntx {
-        use std::mem::transmute;
-
-        let bytes: [u8; 4] = unsafe { transmute(i) };
-        data[0 .. 4].copy_from_slice(&bytes[0 .. 4]);
-
-        let hash = Sha256dHash::from_data(&data);
-        let key = hash.data();
-
-        db.put(&key[..], &data).unwrap();
+    let mut elapsed;
+    for key in &keys {
+        db.put(key, &data).unwrap();
         n += 1;
 
         if n % (bat*tb) == 0 {
             db.batch().unwrap();
-            println!("stored {} txs in {} s", n, now.elapsed().as_secs());
+            elapsed = now.elapsed().as_secs();
+            println!("Stored {} million transactions in {} seconds, {} inserts/second.", n/1000000, elapsed, n/elapsed);
         }
     }
 
     db.batch().unwrap();
-    println!("stored {} txs in {} s", ntx, now.elapsed().as_secs());
+    elapsed = now.elapsed().as_secs();
+    println!("Stored {} million transactions in {} seconds, {} inserts/second ", ntx/1000000, elapsed, ntx/elapsed);
 
+    println!("Reading data ...");
     now = Instant::now();
-    let m = ntx/10;
-    for _ in 1 .. m {
-        use std::mem::transmute;
-
-        let n = rng.next_u32() % ntx;
-        let bytes: [u8; 4] = unsafe { transmute(n) };
-        data[0 .. 4].copy_from_slice(&bytes[0 .. 4]);
-
-        let hash = Sha256dHash::from_data(&data);
-        let key = hash.data();
-
-        if db.get(&key).unwrap().unwrap() != data.to_vec() {
-            println!("failed to store correctly");
-        }
+    for key in &keys {
+        db.get(key).unwrap();
     }
-    println!("{} txs retrieved in {} s", m, now.elapsed().as_secs());
+    elapsed = now.elapsed().as_secs();
+    println!("Read {} million transactions in {} seconds, {} read/second ", ntx/1000000, elapsed, ntx/elapsed);
 
 
     db.shutdown();
