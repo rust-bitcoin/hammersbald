9483034f17221adf37b542e25f6a6571491997b8
Tamas Blummer <tamas.blummer@gmail.com>
2019-06-20 13:51:27 +0200

    pad data files to page boundary at flush with a dummy envelope

--- src/datafile.rs
+++ src/datafile.rs
@@ -111,6 +111,16 @@ impl DataFile {
 
     /// flush buffers
     pub fn flush (&mut self) -> Result<(), HammersbaldError> {
+        let pos = self.appender.position();
+        if pos.in_page_pos() > 0 {
+            if PAGE_SIZE - pos.in_page_pos() >= 4 {
+                let padding = vec!(0u8; PAGE_SIZE - pos.in_page_pos() - 4);
+                self.append_referred(padding.as_slice())?;
+            } else {
+                let padding = vec!(0u8; 2 * PAGE_SIZE - pos.in_page_pos() - 4);
+                self.append_referred(padding.as_slice())?;
+            }
+        }
         self.appender.flush()
     }
 
